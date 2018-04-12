5fdad3f74cc9a6431c4aa4ac13a17b23fdd8c96a
Charlie Sparks <csparks@antioch.edu>
2018-04-12 15:53:06 -0700

    committing after moving file location

--- app.js
+++ app.js
@@ -143,7 +143,7 @@ app.get("/api/restricted", (req, res) => {
 })
 
 //get handler for all notes
-app.get("/api/allNotes", (req, res) => {
+app.get("/api/allNotes", loginMid, (req, res) => {
     NoteModel.find({}, (err, response) => {
         if (err) return console.log(`There was an error getting the notes: ${err}`);
         console.log(`Here are all the notes: \n ${response}`);
